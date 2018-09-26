1f737072ee1def86a587da3f2b7337d2879d27de
Yurii Korolkov <korolkow5@gmail.com>
2018-09-26 14:30:08 +0300

    add info in thread to local server

--- 7_Local_Server/index.js
+++ 7_Local_Server/index.js
@@ -1,9 +1,15 @@
+let fs = require('fs');
+
 let http = require('http');
 
 let server = http.createServer(function (req, res) {
   console.log('URL: ' + req.url);
   res.writeHead(200, {'Content-Type' : 'text/plain; charset=utf-8'});
-  res.end('Hello world');
+
+  let myReadStream = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+
+  myReadStream.pipe(res);
+
 });
 
 server.listen(3000, '127.0.0.1');
