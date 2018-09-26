057cc80d2dc92adfcc03bf29745d403df9c869b7
Yurii Korolkov <korolkow5@gmail.com>
2018-09-26 14:40:51 +0300

    upload html file in thread into local server

--- 7_Local_Server/index.js
+++ 7_Local_Server/index.js
@@ -4,9 +4,11 @@ let http = require('http');
 
 let server = http.createServer(function (req, res) {
   console.log('URL: ' + req.url);
-  res.writeHead(200, {'Content-Type' : 'text/plain; charset=utf-8'});
+  //res.writeHead(200, {'Content-Type' : 'text/plain; charset=utf-8'});
+  res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
 
-  let myReadStream = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+  //let myReadStream = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+  let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
 
   myReadStream.pipe(res);
 
