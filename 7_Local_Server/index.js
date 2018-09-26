61ee97cdbce811737a18bac7c89861db9f3a0c03
Yurii Korolkov <korolkow5@gmail.com>
2018-09-26 15:00:01 +0300

    simple routing

--- 7_Local_Server/index.js
+++ 7_Local_Server/index.js
@@ -5,13 +5,20 @@ let http = require('http');
 let server = http.createServer(function (req, res) {
   console.log('URL: ' + req.url);
   //res.writeHead(200, {'Content-Type' : 'text/plain; charset=utf-8'});
-  res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
 
+  if (req.url === '/index' || req.url === '/') {
+    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res);
+  } else if (req.url === '/about') {
+    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/about.html', 'utf8').pipe(res);
+  } else {
+    res.writeHead(404, {'Content-Type' : 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/404.html', 'utf8').pipe(res);
+  }
   //let myReadStream = fs.createReadStream(__dirname + '/article.txt', 'utf8');
-  let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-
-  myReadStream.pipe(res);
-
+  //let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+  //myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
